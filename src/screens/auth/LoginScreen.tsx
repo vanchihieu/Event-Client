93ad0e62dc28c4d1f375b315cfec519d430a55ad
Van Chi Hieu <vanchihieu3@gmail.com>
2024-07-13 12:44:48 +0700

    react native vector icons

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,10 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import ButtonComponent from '../../components/ButtonComponent';
 
 const LoginScreen = () => {
   return (
     <View>
       <Text>LoginScreen</Text>
+      <ButtonComponent text="Login" type="primary" />
     </View>
   );
 };
