e1fd1f0c6dea889701cec3d3e7e74fe9d5d103f3
Van Chi Hieu <vanchihieu3@gmail.com>
2024-11-14 14:55:16 +0700

    Payment screen

--- src/navigators/MainNavigator.tsx
+++ src/navigators/MainNavigator.tsx
@@ -2,6 +2,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import DrawerNavigator from './DrawerNavigator';
 import {EventDetail, ProfileScreen, SearchEvents} from '../screens';
+import PaymentScreen from '../screens/events/PaymentScreen';
 
 const MainNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -15,6 +16,7 @@ const MainNavigator = () => {
       <Stack.Screen name="EventDetail" component={EventDetail} />
       <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
       <Stack.Screen name="SearchEvents" component={SearchEvents} />
+      <Stack.Screen name="PaymentScreen" component={PaymentScreen} />
     </Stack.Navigator>
   );
 };
